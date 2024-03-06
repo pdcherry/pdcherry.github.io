22390642ec7ee25ab7c971d9a75890cd462c66f3
pdcherry <pcherry@protonmail.ch>
2024-03-05 23:12:17 -0800

    added candidate endorsement analysis

--- posts/2024_03_02-SF_political_support_data/2024_03_02-SF_political_support_scrape_script.R
+++ posts/2024_03_02-SF_political_support_data/2024_03_02-SF_political_support_scrape_script.R
@@ -12,11 +12,9 @@ library(rvest)
 
 fs::dir_info()
 
-
 # Scrape Prop data --------------------------------------------------------
 link <- "https://sfendorsements.com/"
 my_html <- read_html(link)
-class(my_html)
 
 table1 <- html_elements(my_html, "table.table:nth-child(12)") %>%
   html_table()
@@ -40,4 +38,17 @@ html_elements(my_html, "table.table:nth-child(19)") %>%
 org_tbl <- org_scrape[1]
 
 write_csv(prop_table, paste0("./posts/","2024_03_03-props_table.csv"))
+
+
+# Scrape candidate endorsement data ---------------------------------------
+
+table3 <- html_elements(my_html, "table.table:nth-child(8)") %>%
+  html_table()
+
+table4 <- html_elements(my_html, "table.table:nth-child(10)") %>%
+  html_table()
+
+cand_table <- full_join(table3[[1]], table4[[1]], by = "Organization")
+
+write_csv(cand_table, paste0("./posts/2024_03_02-SF_political_support_data/","2024_03_03-cand_table.csv"))
 
